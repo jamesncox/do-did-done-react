df09f9f923b58ef9496d3c22981d216ce3391619
jamesncox <jamesncox@yahoo.com>
2020-07-07 11:53:45 -0500

    Update BottomNavigation styling

--- src/components/Layout/BottomNavigation.js
+++ src/components/Layout/BottomNavigation.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { Link as RouterLink } from 'react-router-dom';
 
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { makeStyles } from '@material-ui/core/styles';
+
 import FolderIcon from '@material-ui/icons/Folder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-
 import HomeIcon from '@material-ui/icons/Home';
 import PersonIcon from '@material-ui/icons/Person';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         width: "100%",
-        margin: "auto",
         position: "fixed",
         bottom: "0",
-        overflow: "hidden"
+        overflow: "hidden",
+        backgroundColor: "#e0e0e0",
+    },
+    icons: {
+        color: "#757575",
+        // color: "white"
     }
-});
+}));
 
 export default function LabelBottomNavigation() {
     const classes = useStyles();
@@ -30,10 +34,10 @@ export default function LabelBottomNavigation() {
 
     return (
         <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
-            <BottomNavigationAction label="Home" value="home" component={RouterLink} to="/" icon={<HomeIcon />} />
-            <BottomNavigationAction label="Profile" value="profile" component={RouterLink} to="/Profile" icon={<PersonIcon />} />
-            <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+            <BottomNavigationAction className={classes.icons} label="Home" value="home" component={RouterLink} to="/" icon={<HomeIcon />} />
+            <BottomNavigationAction className={classes.icons} label="Profile" value="profile" component={RouterLink} to="/Profile" icon={<PersonIcon />} />
+            <BottomNavigationAction className={classes.icons} label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction className={classes.icons} label="Folder" value="folder" icon={<FolderIcon />} />
         </BottomNavigation>
     );
 }
