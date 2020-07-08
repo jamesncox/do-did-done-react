c043be60a307f8aa1cd316dca68c1755889a6882
jamesncox <jamesncox@yahoo.com>
2020-07-08 16:57:33 -0500

    Remove filler text from Profile.js

--- src/components/Layout/Profile.js
+++ src/components/Layout/Profile.js
@@ -40,9 +40,7 @@ function Profile(props) {
                 <Typography className={classes.header}>
                     Welcome, {props.user.username}
                 </Typography>
-                <Typography>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
-                </Typography>
+
                 <BottomNavigation />
             </div>
         )
