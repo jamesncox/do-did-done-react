f87ce72efe4546c17eaf252a151e90c314d6d6f4
jamesncox <jamesncox@yahoo.com>
2020-07-08 10:35:46 -0500

    Import Page.js to Profile.js

--- src/components/Layout/Profile.js
+++ src/components/Layout/Profile.js
@@ -2,30 +2,11 @@ import React from 'react'
 
 import Header from './Header'
 import BottomNavigation from './BottomNavigation'
+import Page from './Page'
 
-import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    topDivider: {
-        backgroundColor: "#81d4fa",
-        opacity: "50%",
-        marginTop: theme.spacing(6),
-        marginBottom: theme.spacing(5),
-    },
-    blueDivider: {
-        backgroundColor: "#81d4fa",
-        opacity: "50%",
-        marginBottom: theme.spacing(5),
-    },
-    redDivider: {
-        backgroundColor: "#ff1744",
-        opacity: "20%",
-        marginLeft: "4rem",
-        position: "absolute",
-        top: "0"
-    }
-
 }))
 
 
@@ -34,26 +15,7 @@ function Profile(props) {
     return (
         <>
             <Header />
-            <Divider className={classes.redDivider} orientation="vertical" />
-            <Divider className={classes.topDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
-            <Divider className={classes.blueDivider} />
+            <Page />
             <BottomNavigation />
         </>
     )
