a2a1b653852a2fe04038d9d16f17548b37bedd51
jamesncox <jamesncox@yahoo.com>
2020-07-09 09:17:48 -0500

    Create reducers/categories add category actions

---
+++ src/reducers/categories.js
@@ -0,0 +1,33 @@
+import {
+    SET_CATEGORIES,
+    LOADING_CATEGORIES,
+    CREATE_CATEGORY,
+    UPDATE_CATEGORY,
+    DELETE_CATEGORY,
+    LOADING_SINGLE_CATEGORY
+} from '../actionTypes'
+
+export default (state = {
+    categories: [],
+    loadingCategories: false,
+    errors: null,
+    categoryId: null,
+    loadingSingleCategory: false
+}, action) => {
+    switch (action.type) {
+        case LOADING_CATEGORIES:
+            return { ...state, categories: [...state.categories], loadingCategories: true }
+
+        case LOADING_SINGLE_CATEGORY:
+            return { ...state, categories: [...state.categories], loadingSingleCategory: true }
+
+        case SET_CATEGORIES:
+            return { ...state, categories: action.payload, loadingCategories: false }
+
+        case CREATE_CATEGORY:
+            return { ...state, categories: [...state.categories, action.payload], loadingSingleCategory: false }
+
+        default:
+            return state
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import sessions from '../reducers/sessions'
 import users from '../reducers/users'
+import categoires from '../reducers/categories'
 
 const rootReducer = combineReducers(
     {
         sessions,
         users,
+        categories
     }
 )
 
